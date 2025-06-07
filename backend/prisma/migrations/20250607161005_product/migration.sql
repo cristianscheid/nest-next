64d1d2ac1133b272c30baa61632965f9afd01814
Cristian Scheid <cristianscheid@gmail.com>
2025-06-07 13:42:27 -0300

    fix(backend): update schema for optional description field

--- backend/prisma/migrations/20250607161005_product/migration.sql
+++ backend/prisma/migrations/20250607161005_product/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "products" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
+    "description" TEXT,
     "price" DOUBLE PRECISION NOT NULL,
 
     CONSTRAINT "products_pkey" PRIMARY KEY ("id")
