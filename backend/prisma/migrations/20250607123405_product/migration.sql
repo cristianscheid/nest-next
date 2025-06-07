7e4f7ccae589837111269896b1e4cd3629d2fe99
Cristian Scheid <cristianscheid@gmail.com>
2025-06-07 09:37:03 -0300

    feat(backend): update Prisma schema mapping

--- backend/prisma/migrations/20250607123405_product/migration.sql
+++ backend/prisma/migrations/20250607123405_product/migration.sql
@@ -1,9 +1,9 @@
 -- CreateTable
-CREATE TABLE "Product" (
+CREATE TABLE "products" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "price" DOUBLE PRECISION NOT NULL,
 
-    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
 );
