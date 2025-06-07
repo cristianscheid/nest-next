0ec8b6d9b3cd308713fe43c6e09a56153c5283ad
Cristian Scheid <cristianscheid@gmail.com>
2025-06-07 09:23:03 -0300

    refactor(backend): clean up AppModule by removing unused controllers and providers

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DbModule } from './db/db.module';
 import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [DbModule, ProductModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,4 +5,5 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(process.env.PORT ?? 4000);
 }
+
 bootstrap();
