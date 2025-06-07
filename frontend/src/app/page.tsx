b89401ea6fcae369bd2324f2340f0a339beba103
Cristian Scheid <cristianscheid@gmail.com>
2025-06-07 10:03:51 -0300

    feat(frontend): implement product management features with create, update, delete, and fetch functionality

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,103 +1,143 @@
-import Image from "next/image";
+'use client'
+
+import {useEffect, useState} from 'react'
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL
+
+type Product = {
+    id?: number
+    name?: string
+    description?: string
+    price?: number
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    const [product, setProduct] = useState<Product>({})
+    const [products, setProducts] = useState<Product[]>([])
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    useEffect(() => {
+        fetchProducts()
+    }, [])
+
+    async function fetchProducts() {
+        const resp = await fetch(`${API_URL}/products`)
+        const products = await resp.json()
+        setProducts(products)
+    }
+
+    async function createProduct() {
+        await fetch(`${API_URL}/products`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(product),
+        })
+        setProduct({})
+        await fetchProducts()
+    }
+
+    async function updateProduct() {
+        await fetch(`${API_URL}/products/${product.id}`, {
+            method: 'PATCH',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(product),
+        })
+        setProduct({})
+        await fetchProducts()
+    }
+
+    async function fetchProductById(id: number) {
+        const resp = await fetch(`${API_URL}/products/${id}`)
+        const product = await resp.json()
+        setProduct(product)
+    }
+
+    async function deleteProduct(id: number) {
+        await fetch(`${API_URL}/products/${id}`, {method: 'DELETE'})
+        await fetchProducts()
+    }
+
+    function renderProductForm() {
+        return (
+            <div className="flex gap-5 items-end">
+                <div className="flex flex-col">
+                    <label htmlFor="name">Name</label>
+                    <input
+                        id="name"
+                        type="text"
+                        value={product.name ?? ''}
+                        onChange={(e) => setProduct({...product, name: e.target.value})}
+                        className="bg-zinc-700 p-2 rounded-md"
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label htmlFor="description">Description</label>
+                    <input
+                        id="description"
+                        type="text"
+                        value={product.description ?? ''}
+                        onChange={(e) => setProduct({...product, description: e.target.value})}
+                        className="bg-zinc-700 p-2 rounded-md"
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label htmlFor="price">Price</label>
+                    <input
+                        id="price"
+                        type="number"
+                        value={product.price ?? ''}
+                        onChange={(e) => setProduct({...product, price: +e.target.value})}
+                        className="bg-zinc-700 p-2 rounded-md"
+                    />
+                </div>
+                <div>
+                    {product.id ? (
+                        <button onClick={updateProduct} className="bg-blue-500 px-4 py-2 rounded-md">
+                            Update Product
+                        </button>
+                    ) : (
+                        <button onClick={createProduct} className="bg-blue-500 px-4 py-2 rounded-md">
+                            Create Product
+                        </button>
+                    )}
+                </div>
+            </div>
+        )
+    }
+
+    function renderProducts() {
+        return (
+            <div className="flex flex-col gap-2">
+                {products.map((product) => (
+                    <div
+                        key={product.id}
+                        className="flex items-center gap-2 bg-zinc-800 px-4 py-2 rounded-md"
+                    >
+                        <div className="flex-1">{product.name}</div>
+                        <div>{product.price}</div>
+                        <div>
+                            <button
+                                onClick={() => fetchProductById(product.id!)}
+                                className="bg-green-500 p-2 rounded-md"
+                            >
+                                Edit
+                            </button>
+                            <button
+                                onClick={() => deleteProduct(product.id!)}
+                                className="bg-red-500 p-2 rounded-md"
+                            >
+                                Delete
+                            </button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        )
+    }
+
+    return (
+        <div className="flex flex-col justify-center items-center h-screen gap-10">
+            {renderProductForm()}
+            {renderProducts()}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    )
 }
